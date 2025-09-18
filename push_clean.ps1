425dd052a1992df391bd1479e0675cba998d3c50
toureast25 <viatheslav96@mail.ru>
2025-09-18 17:35:34 +1000

    Delete push_clean.ps1

--- push_clean.ps1
+++
@@ -1,9 +0,0 @@
-# Скрипт для загрузки чистого репозитория
-& 'C:\Program Files\Git\bin\git.exe' add .
-& 'C:\Program Files\Git\bin\git.exe' commit -m "Clean repository: only main script and README"
-& 'C:\Program Files\Git\bin\git.exe' remote add origin https://github.com/toureast25/git-1c-manager.git
-& 'C:\Program Files\Git\bin\git.exe' branch -M main
-& 'C:\Program Files\Git\bin\git.exe' push -f origin main
-& 'C:\Program Files\Git\bin\git.exe' tag -a v1.0.0 -m "Release v1.0.0 - Git-1C Manager"
-& 'C:\Program Files\Git\bin\git.exe' push origin v1.0.0
-Write-Host "Clean repository uploaded successfully!"
